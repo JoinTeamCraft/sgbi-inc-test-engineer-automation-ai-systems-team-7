81f31f4f43b324a02ab48cb8740a1c86906b2670
sreeresmims <sreeresmims99@gmail.com>
2026-02-22 18:51:10 +0530

    Update env_config.py
    [SE-8]

--- config/env_config.py
+++ config/env_config.py
@@ -1,12 +1,23 @@
 import os
 
+
 class EnvConfig:
-    """
-    Template for environment configuration.
-    """
-    
     ROBOT_LIBRARY_SCOPE = 'GLOBAL'
 
-    def get_config_value(self):
-        """Placeholder for config retrieval."""
-        pass
+    def __init__(self):
+        self.base_url = os.getenv("BASE_URL", "https://morent-car.archisacademy.com/")
+        self.browser = os.getenv("BROWSER", "chrome")
+        self.default_timeout = os.getenv("DEFAULT_TIMEOUT", "20s")
+        self.headless = os.getenv("HEADLESS", "false").lower() == "true"
+
+    def get_base_url(self):
+        return self.base_url
+
+    def get_browser(self):
+        return self.browser
+
+    def get_default_timeout(self):
+        return self.default_timeout
+
+    def is_headless(self):
+        return self.headless
